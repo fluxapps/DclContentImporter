77bf09b1792ac0b2731b7d5c380b595148d2cb1c
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2022-03-21 15:55:12 -0400

    updated to ilias 7"

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = 'sr_dcl_importer';
 $version = '1.0.0';
 $ilias_min_version = '6.0';
-$ilias_max_version = '6.99';
+$ilias_max_version = '7.99';
 $responsible = 'Michael Herren';
 $responsible_mail = 'mh@studer-raimann.ch';
